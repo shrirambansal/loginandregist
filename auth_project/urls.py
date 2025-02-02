0e54f712d73841dea43e04d26d5d0a8f76a4200b
shrirambansal <shrirambansal13@gmail.com>
2025-02-02 19:23:07 +0530

    Add root

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
+    path('', TemplateView.as_view(template_name="auth/login.html")),  # Add this line to serve the root page
+
 ]
